be9a17ce7e8a6ae5f53f18fa2015d406bac0b19f
Nicholas Devenish <ndevenish@gmail.com>
2021-08-23 10:58:39 +0100

    Start working out some interface

--- h5read_rust/src/lib.rs
+++ h5read_rust/src/lib.rs
@@ -1,3 +1,35 @@
+use libc::c_char;
+use std::ffi::CStr;
+
+pub struct H5File {
+    filename: String,
+    master_file: hdf5::File,
+}
+
+#[no_mangle]
+pub extern "C" fn h5read_open(filename: *const c_char) -> *mut H5File {
+    let c_filename = unsafe {
+        assert!(!filename.is_null());
+        CStr::from_ptr(filename)
+    };
+    let r_filename = c_filename.to_str().unwrap().to_owned();
+    if let Ok(file) = hdf5::File::open(&r_filename) {
+        Box::into_raw(Box::new(H5File {
+            filename: r_filename,
+            master_file: file,
+        }))
+    } else {
+        std::ptr::null_mut()
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn h5read_free(obj: *mut H5File) {
+    let obj = unsafe {
+        Box::from_raw(obj);
+    };
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
