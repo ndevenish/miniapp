a31d89c4f191d65941915745e39fc58f3d76079b
Nicholas Devenish <ndevenish@gmail.com>
2021-09-06 15:50:12 +0100

    Remove SYCL part to start again

--- dpcpp/initial/load.cc
+++ dpcpp/initial/load.cc
@@ -38,11 +38,16 @@ int main(int argc, char** argv) {
     uint16_t* module_data = malloc_shared<uint16_t>(num_pixels, Q);
 
     // Print information about the device we are using
-    std::cout << "Using Device: " << BOLD
+    std::string device_kind = Q.get_device().is_cpu()           ? "CPU"
+                              : Q.get_device().is_gpu()         ? "GPU"
+                              : Q.get_device().is_accelerator() ? "FPGA"
+                                                                : "Unknown";
+    std::cout << "Using " << BOLD << device_kind << NC << " Device: " << BOLD
               << Q.get_device().get_info<info::device::name>() << NC << std::endl;
 
     std::cout << "Module size s,f: " << modules->slow << ", " << modules->fast
               << std::endl;
+
     // Count the zeros in our modules data on-host
     size_t host_zeros = 0;
     for (int i = 0; i < std::min(modules->slow, (size_t)512); ++i) {
@@ -59,29 +64,30 @@ int main(int argc, char** argv) {
     // Copy our module to the shared buffer
     std::copy(modules->data, modules->data + num_pixels, module_data);
 
-    int sum = 0;
-    buffer buf_sum(&sum);
-    int slow = modules->slow;
-    int fast = modules->fast;
-    auto module_size = range<2>{512, 1024};  // modules->slow, modules->fast};
-#ifdef FPGA
-    auto module_range = module_size;
-#else
-    auto module_range = range<2>{64, 64};
-#endif
-    Q.submit([&](handler& h) {
-        h.parallel_for(nd_range(module_size, module_range), [=](nd_item<2> idx) {
-            accessor sum(buf_sum, h, write_only);
-            int y = idx.get_global_id()[0];
-            int x = idx.get_global_id()[1];
-            if (module_data[y * fast + x] == 0) {
-                sum[0] += 1;
-            }
-        });
-    });
-    Q.wait();
-    auto col = sum == host_zeros ? G : R;
-    std::cout << "Pixel count from kernel:           " << col << sum << NC << std::endl;
+    // int sum = 0;
+    // buffer buf_sum(&sum);
+    // int slow = modules->slow;
+    // int fast = modules->fast;
+    //     auto module_size = range<2>{512, 1024};  // modules->slow, modules->fast};
+    // #ifdef FPGA
+    //     auto module_range = module_size;
+    // #else
+    //     auto module_range = range<2>{64, 64};
+    // #endif
+    //     Q.submit([&](handler& h) {
+    //         h.parallel_for(nd_range(module_size, module_range), [=](nd_item<2> idx) {
+    //             accessor sum(buf_sum, h, write_only);
+    //             int y = idx.get_global_id()[0];
+    //             int x = idx.get_global_id()[1];
+    //             if (module_data[y * fast + x] == 0) {
+    //                 sum[0] += 1;
+    //             }
+    //         });
+    //     });
+    //     Q.wait();
+    //     auto col = sum == host_zeros ? G : R;
+    //     std::cout << "Pixel count from kernel:           " << col << sum << NC <<
+    //     std::endl;
 
     free(module_data, Q);
     h5read_free_image_modules(modules);
