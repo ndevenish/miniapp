90459cd9a15981e58b850289d8ce44bcc057c2cc
Nicholas Devenish <ndevenish@gmail.com>
2021-10-27 12:11:19 +0100

    Move mini stream test back to 256 bit for now

--- dpcpp/mini_stream_test/ministream.cc
+++ dpcpp/mini_stream_test/ministream.cc
@@ -19,7 +19,8 @@ constexpr size_t E2XE_16M_FAST = 4148;
 // This is a single block of pixels read every tick
 // 32 = 512 bit
 // 16 = 256 bit
-using PipedPixelsArray = std::array<uint16_t, 8>;
+//  8 = 128 bit
+using PipedPixelsArray = std::array<uint16_t, 16>;
 
 class Producer;
 
