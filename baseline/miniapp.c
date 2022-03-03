0a9e215f497f48974f3e43767e25383d760014a5
Matthew Pritchard <matthew.pritchard@diamond.ac.uk>
2022-03-03 12:31:13 +0000

    Further tidying of miniapp.c

--- baseline/miniapp.c
+++ baseline/miniapp.c
@@ -9,6 +9,122 @@
 #include "h5read.h"
 #include "eiger2xe.h"
 
+void time_image_loading(h5read_handle* obj, int n_images);
+
+double time_parallelism_over_images(h5read_handle *obj, int n_images, void** spotfinders, int* full_results);
+
+double time_parallelism_over_images_using_modules(h5read_handle *obj, int n_images, int n_modules, void** mini_spotfinders, int* full_results_m);
+
+double time_parallelism_over_modules(h5read_handle* obj, int n_images, int n_modules, void** mini_spotfinders, int* mini_results);
+
+double time_parallelism_over_modules_using_floats(h5read_handle* obj, int n_images, int n_modules, void** mini_spotfinders_f, int* mini_results_f);
+
+double time_parallelism_over_both(h5read_handle* obj, int n_images, int n_modules, void** mini_spotfinders, int* both_results);
+
+int old_main(h5read_handle* obj, int n_images);
+
+int module_to_image_index(int module_num, int module_idx);
+
+int main(int argc, char **argv) {
+
+    h5read_handle *obj = h5read_parse_standard_args(argc, argv);
+    size_t n_images = h5read_get_number_of_images(obj);
+    image_modules_t *modules = h5read_get_image_modules(obj, 0);
+
+    size_t image_fast_size = E2XE_16M_FAST;
+    size_t image_slow_size = E2XE_16M_SLOW;
+    size_t module_fast_size = E2XE_MOD_FAST;
+    size_t module_slow_size = E2XE_MOD_SLOW;
+    size_t n_modules = E2XE_16M_NSLOW * E2XE_16M_NFAST; //modules->modules;
+    printf("Num modules: %d\n", n_modules);
+
+    int num_spotfinders;
+    int thread_num;
+#ifdef _OPENMP
+    double tstart = omp_get_wtime();
+    printf("OMP found; have %d threads\n", omp_get_max_threads());
+    num_spotfinders = omp_get_max_threads();
+#endif
+#ifndef _OPENMP
+    num_spotfinders = 1;
+#endif
+    void* mini_spotfinders[num_spotfinders];
+    void* mini_spotfinders_f[num_spotfinders];
+    void* spotfinders[num_spotfinders];
+    for (size_t j=0; j<num_spotfinders; j++) {
+        mini_spotfinders[j] = spotfinder_create(module_fast_size, module_slow_size);
+        mini_spotfinders_f[j] = spotfinder_create_f(module_fast_size, module_slow_size);
+        spotfinders[j] = spotfinder_create(image_fast_size, image_slow_size);
+    }
+    int test_count = 0;
+    image_t* image;
+    int temp;
+    if (argc > 2) {
+        n_images = (sscanf(argv[2], "%d", &temp) == 1) ? temp : n_images;
+    }
+
+    printf("Finding spots in %d images\n", n_images);
+
+    time_image_loading(obj, n_images);
+
+    int full_results[n_images];
+    int mini_results[n_images];
+    int mini_f_results[n_images];
+    int both_results[n_images];
+    int full_results_m[n_images];
+
+    double over_images_time = time_parallelism_over_images(obj, n_images, spotfinders, full_results);
+    double over_images_using_modules_time = time_parallelism_over_images_using_modules(obj, n_images, n_modules, mini_spotfinders, full_results_m);
+    double over_modules_time = time_parallelism_over_modules(obj, n_images, n_modules, mini_spotfinders, mini_results);
+    double over_modules_using_floats_time = time_parallelism_over_modules_using_floats(obj, n_images, n_modules, mini_spotfinders, mini_f_results);
+    double over_both_time = time_parallelism_over_both(obj, n_images, n_modules, mini_spotfinders, both_results);
+
+    for (size_t j=0; j<num_spotfinders; j++) {
+        spotfinder_free(mini_spotfinders[j]);
+        spotfinder_free(mini_spotfinders_f[j]);
+        spotfinder_free(spotfinders[j]);
+    }
+
+    h5read_free(obj);
+
+    printf(
+        "\nTime to run with parallel over:\n\
+        Images with modules: %4.0f ms/image\n\
+        Images:              %4.0f ms/image\n\
+        Modules:             %4.0f ms/image\n\
+        Modules (float):     %4.0f ms/image\n\
+        Both:                %4.0f ms/image\n",
+        over_images_using_modules_time / n_images * 1000,
+        over_images_time / n_images * 1000,
+        over_modules_time / n_images * 1000,
+        over_modules_using_floats_time / n_images * 1000,
+        over_both_time / n_images * 1000
+    );
+
+    printf("\nStrong pixels count results:\n");
+    printf("Img# Images Modules (float)  Both\n");
+    for (size_t j = 0; j < 5; j++) {
+        char *col = "\033[1;31m";
+        if (omp_get_max_threads() % 2 == 0){
+            if (full_results[j] == mini_results[j] && full_results[j] == both_results[j] && full_results[j] == full_results_m[j]) {
+                col = "\033[32m";
+            }
+        } else if (full_results[j] == mini_results[j] && full_results[j] == full_results_m[j]) {
+            col = "\033[32m";
+        }
+        printf("%s%4d %6d %7d \033[0m%6d%s %5d\n\033[0m",
+               col,
+               j,
+               full_results[j],
+               mini_results[j],
+               mini_f_results[j],
+               col,
+               both_results[j]);
+    }
+
+    return 0;
+}
+
 void time_image_loading(h5read_handle* obj, int n_images) {
     image_modules_t* modules;
     image_t* image;
@@ -116,109 +232,22 @@ double time_parallelism_over_both(h5read_handle* obj, int n_images, int n_module
             both_results[j] = strong_pixels_from_modules;
         }
     } else {
-        for (size_t j=0; j<n_images; j++) {
-            both_results[j] = 0;
-        }
+        for (size_t j=0; j<n_images; j++) both_results[j] = 0;
     }
     return omp_get_wtime() - t0;
 }
 
-int main(int argc, char **argv) {
-
-    h5read_handle *obj = h5read_parse_standard_args(argc, argv);
-    size_t n_images = h5read_get_number_of_images(obj);
-    image_modules_t *modules = h5read_get_image_modules(obj, 0);
-
-    size_t image_fast_size = E2XE_16M_FAST;
-    size_t image_slow_size = E2XE_16M_SLOW;
-    size_t module_fast_size = E2XE_MOD_FAST;
-    size_t module_slow_size = E2XE_MOD_SLOW;
-    size_t n_modules = E2XE_16M_NSLOW * E2XE_16M_NFAST; //modules->modules;
-    printf("Num modules: %d\n", n_modules);
-
-    int num_spotfinders;
-    int thread_num;
-#ifdef _OPENMP
-    double tstart = omp_get_wtime();
-    printf("OMP found; have %d threads\n", omp_get_max_threads());
-    num_spotfinders = omp_get_max_threads();
-#endif
-#ifndef _OPENMP
-    num_spotfinders = 1;
-#endif
-    void* mini_spotfinders[num_spotfinders];
-    void* mini_spotfinders_f[num_spotfinders];
-    void* spotfinders[num_spotfinders];
-    for (size_t j=0; j<num_spotfinders; j++) {
-        mini_spotfinders[j] = spotfinder_create(module_fast_size, module_slow_size);
-        mini_spotfinders_f[j] = spotfinder_create_f(module_fast_size, module_slow_size);
-        spotfinders[j] = spotfinder_create(image_fast_size, image_slow_size);
-    }
-    int test_count = 0;
+int old_main(h5read_handle* obj, int n_images) {
     image_t* image;
-    int temp;
-    if (argc > 2) {
-        n_images = (sscanf(argv[2], "%d", &temp) == 1) ? temp : n_images;
-    }
-
-    printf("Finding spots in %d images\n", n_images);
-
-    time_image_loading(obj, n_images);
-
-    int full_results[n_images];
-    int mini_results[n_images];
-    int mini_f_results[n_images];
-    int both_results[n_images];
-    int full_results_m[n_images];
-
-    double over_images_time = time_parallelism_over_images(obj, n_images, spotfinders, full_results);
-    double over_images_using_modules_time = time_parallelism_over_images_using_modules(obj, n_images, n_modules, mini_spotfinders, full_results_m);
-    double over_modules_time = time_parallelism_over_modules(obj, n_images, n_modules, mini_spotfinders, mini_results);
-    double over_modules_using_floats_time = time_parallelism_over_modules(obj, n_images, n_modules, mini_spotfinders, mini_f_results);
-    double over_both_time = time_parallelism_over_both(obj, n_images, n_modules, mini_spotfinders, both_results);
-
-    printf(
-      "\nTime to run with parallel over:\n\
-Images with modules: %4.0f ms/image\n\
-Images:              %4.0f ms/image\n\
-Modules:             %4.0f ms/image\n\
-Modules (float):     %4.0f ms/image\n\
-Both:                %4.0f ms/image\n",
-      over_images_using_modules_time / n_images * 1000,
-      over_images_time / n_images * 1000,
-      over_modules_time / n_images * 1000,
-      over_modules_using_floats_time / n_images * 1000,
-      over_both_time / n_images * 1000);
-
-    printf("\nStrong pixels count results:\n");
-    printf("Img# Images Modules (float)  Both\n");
-    for (size_t j = 0; j < 5; j++) {
-        char *col = "\033[1;31m";
-        if (omp_get_max_threads() % 2 == 0){
-            if (full_results[j] == mini_results[j] && full_results[j] == both_results[j] && full_results[j] == full_results_m[j]) {
-                col = "\033[32m";
-            }
-        } else if (full_results[j] == mini_results[j] && full_results[j] == full_results_m[j]) {
-            col = "\033[32m";
-        }
-        printf("%s%4d %6d %7d \033[0m%6d%s %5d\n\033[0m",
-               col,
-               j,
-               full_results[j],
-               mini_results[j],
-               mini_f_results[j],
-               col,
-               both_results[j]);
-    }
-
-    uint16_t image_slow = 0, image_fast = 0;
+    image_modules_t* modules;
+    uint32_t strong_pixels;
+    size_t zero;
+    size_t zero_m;
+    uint16_t image_slow = E2XE_16M_SLOW, image_fast = E2XE_16M_FAST;
     void *spotfinder = NULL;
-    size_t modules_true, image_true;
-    for (size_t j = 0; j < 1; j++) {
+    for (size_t j = 0; j < n_images; j++) {
         image = h5read_get_image(obj, j);
         modules = h5read_get_image_modules(obj, j);
-        modules_true=0;
-        image_true=0;
 
         if (j == 0) {
             // Need to wait until we have an image to get its size
@@ -232,41 +261,10 @@ Both:                %4.0f ms/image\n",
             assert(image->fast == image_fast);
         }
 
-        for (size_t n=0; n<modules->modules; n++) {
-            size_t i_fast = n % 4;
-            size_t i_slow = n / 4;
-            size_t r_0 = i_slow * (modules->slow + 38) * image_fast;
-            for (size_t k=0; k<modules->fast*modules->slow; k++) {
-                size_t offset = r_0 + (k / modules->fast) * image->fast + i_fast *(modules->fast+12);
-                size_t idx = offset + k%modules->fast;
-                if (modules->data[n*(modules->fast*modules->slow)+k] != image->data[idx]) {
-                    printf("%d %d %d\n", k/modules->fast, k%modules->fast, offset);
-                    printf("Module %d: non-equal at %d-%d: %d %d\n", n, n*(modules->fast*modules->slow)+k, idx, modules->data[n*(modules->fast*modules->slow)+k], image->data[idx]);
-                    break;//exit(1);
-                }
-            }
-        }
-
-        for (size_t n=0; n<modules->modules; n++) {
-            size_t i_fast = n % 4;
-            size_t i_slow = n / 4;
-            size_t r_0 = i_slow * (modules->slow + 38) * image_fast;
-            modules_true = 0;
-            image_true = 0;
-            for (size_t k=0; k<modules->fast*modules->slow; k++) {
-                size_t offset = r_0 + (k / modules->fast) * image->fast + i_fast *(modules->fast+12);
-                size_t idx = offset + k%modules->fast;
-                if (modules->mask[n*(modules->fast*modules->slow)+k] != image->mask[idx]) {
-                    printf("Mask disagreement at %d %d\n", n*(modules->fast*modules->slow)+k, idx);
-                }
-            }
-        }
-
         uint32_t strong_pixels;
         size_t zero;
         size_t zero_m;
 
-        // for (int l = 0; l < 100; l++) {
         strong_pixels = spotfinder_standard_dispersion(spotfinder, image);
 
         zero = 0;
@@ -283,7 +281,6 @@ Both:                %4.0f ms/image\n",
                 zero_m++;
             }
         }
-        // }
 
         printf("image %ld had %ld / %ld valid zero pixels, %" PRIu32 " strong pixels\n\n",
                j,
@@ -291,20 +288,17 @@ Both:                %4.0f ms/image\n",
                zero_m,
                strong_pixels);
 
-        // h5read_free_image_modules(modules);
+        h5read_free_image_modules(modules);
         h5read_free_image(image);
     }
-
-    for (size_t j=0; j<num_spotfinders; j++) {
-        spotfinder_free(mini_spotfinders[j]);
-        spotfinder_free(mini_spotfinders_f[j]);
-        spotfinder_free(spotfinders[j]);
-    }
-
-    h5read_free_image_modules(modules);
-    //spotfinder_free(mini_spotfinder);
     spotfinder_free(spotfinder);
-    h5read_free(obj);
+}
 
-    return 0;
+int module_to_image_index(int module_num, int module_idx) {
+    size_t i_fast = module_num % E2XE_16M_NFAST;
+    size_t i_slow = module_num / E2XE_16M_NFAST;
+    size_t r_0 = i_slow * (E2XE_MOD_SLOW + E2XE_GAP_SLOW) * E2XE_16M_FAST;
+    size_t offset = r_0 + (module_idx / E2XE_MOD_FAST) * E2XE_16M_FAST + i_fast *(E2XE_MOD_FAST + E2XE_GAP_FAST);
+    size_t image_idx = offset + module_idx % E2XE_MOD_FAST;
+    return image_idx;
 }
