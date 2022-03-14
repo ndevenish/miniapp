8a9b09c3ae0e6396b707defec7353660709f323d
Matthew Pritchard <matthew.pritchard@diamond.ac.uk>
2022-03-14 10:41:05 +0000

    Add a baseline

--- baseline/miniapp.c
+++ baseline/miniapp.c
@@ -31,6 +31,22 @@ void time_image_loading(h5read_handle* obj, int n_images) {
             1000*(t2-t1)/n_images);
 }
 
+double time_baselines(h5read_handle* obj,
+                                    int n_images,
+                                    void** spotfinders,
+                                    int* full_results) {
+    int temp;
+    image_t* image;
+    double t0 = omp_get_wtime();
+    for (size_t j = 0; j < n_images; j++) {
+        image = h5read_get_image(obj, j);
+        temp = spotfinder_standard_dispersion(spotfinders[omp_get_thread_num()], image);
+        h5read_free_image(image);
+        full_results[j] = temp;
+    }
+    return omp_get_wtime() - t0;
+}
+
 double time_parallelism_over_images(h5read_handle *obj, int n_images, void** spotfinders, int* full_results) {
     int temp;
     image_t* image;
@@ -195,12 +211,14 @@ int main(int argc, char **argv) {
 
     // time_image_loading(obj, n_images);
 
+    int baseline_results[n_images];
     int full_results[n_images];
     int mini_results[n_images];
     int both_results[n_images];
     int full_results_m[n_images];
     int both_results_nb[n_images];
-
+    
+    double baseline_time = time_baselines(obj, n_images, spotfinders, baseline_results);
     double over_images_time = time_parallelism_over_images(obj, n_images, spotfinders, full_results);
     double over_images_using_modules_time = time_parallelism_over_images_using_modules(obj, n_images, n_modules, mini_spotfinders, full_results_m);
     double over_modules_time = time_parallelism_over_modules(obj, n_images, n_modules, mini_spotfinders, mini_results);
@@ -217,13 +235,15 @@ int main(int argc, char **argv) {
 
     printf(
         "\nTime to run with parallel over:\n\
-        Images with modules:           %4.0f ms/image\n\
+        Nothing (baseline)             %4.0f ms/image\n\
         Images:                        %4.0f ms/image\n\
+        Images with modules:           %4.0f ms/image\n\
         Modules:                       %4.0f ms/image\n\
         Both:                          %4.0f ms/image\n\
         Both (no blit):                %4.0f ms/image\n",
-        over_images_using_modules_time / n_images * 1000,
+        baseline_time / n_images * 1000,
         over_images_time / n_images * 1000,
+        over_images_using_modules_time / n_images * 1000,
         over_modules_time / n_images * 1000,
         over_both_time / n_images * 1000,
         over_both_noblit_time / n_images * 1000
