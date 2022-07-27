57f817c316b3f3ba0fe0679b69b45d2c6c747843
Nicholas Devenish <ndevenish@gmail.com>
2022-07-27 18:26:07 +0100

    Use wrapping on argparse

--- dpcpp/init/init.cc
+++ dpcpp/init/init.cc
@@ -6,8 +6,31 @@
 
 using namespace sycl;
 
-int main(int argc, char** argv) {
-    auto args = FPGAArgumentParser().parse_args(argc, argv);
+int main(int argc, char **argv) {
+    auto parser = FPGAArgumentParser();
+    auto &group = parser.add_mutually_exclusive_group(false);
+    group.add_argument("--sample")
+      .help(
+        "Don't load a data file, instead use generated test data. If "
+        "H5READ_IMPLICIT_SAMPLE is set, then this is assumed, if a file is not "
+        "provided.")
+      .implicit_value(true);
+    group.add_argument("file")
+      .metavar("FILE.nxs")
+      .help("Path to the Nexus file to parse")
+      .action([](const std::string &value) { fmt::print("Action FILE: {}", value); });
+
+    auto args = parser.parse_args(argc, argv);
 
     return 0;
 }
+
+/*
+      "Options:\n\
+  FILE.nxs      Path to the Nexus file to parse\n\
+  -h, --help    Show this message\n\
+  -v            Verbose HDF5 message output\n\
+  --sample      Don't load a data file, instead use generated test data.\n\
+                If H5READ_IMPLICIT_SAMPLE is set, then this is assumed,\n\
+                if a file is not provided.";
+*/
