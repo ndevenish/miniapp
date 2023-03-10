5c8d90701f9142062f1af62cc60cbb8850f0f930
Nicholas Devenish <ndevenish@gmail.com>
2023-03-10 13:55:49 +0000

    Start adapting spotfind_naive to block images

--- cuda/spotfind_naive/spotfind_naive.cu
+++ cuda/spotfind_naive/spotfind_naive.cu
@@ -108,7 +108,18 @@ int main(int argc, char **argv) {
     // Parse arguments and get our H5Reader
     auto parser = CUDAArgumentParser();
     parser.add_h5read_arguments();
+    parser.add_argument("--batch")
+      .help("Batch images into chunks for processing")
+      .default_value<uint32_t>(1)
+      .metavar("NUM")
+      .scan<'u', uint32_t>();
+
     auto args = parser.parse_args(argc, argv);
+    uint32_t batch_size = parser.get<uint32_t>("batch");
+    if (batch_size < 1) {
+        print("Error: Batch size must be >= 1\n");
+        std::exit(1);
+    }
 
     auto reader = args.file.empty() ? H5Read() : H5Read(args.file);
 
@@ -119,36 +130,53 @@ int main(int argc, char **argv) {
     dim3 thread_block_size{32, 16};
     dim3 blocks_dims{
       static_cast<unsigned int>(ceilf((float)width / thread_block_size.x)),
-      static_cast<unsigned int>(ceilf((float)height / thread_block_size.y))};
+      static_cast<unsigned int>(ceilf((float)height / thread_block_size.y)),
+      batch_size};
     const int num_threads_per_block = thread_block_size.x * thread_block_size.y;
-    const int num_blocks = blocks_dims.x * blocks_dims.y;
-    print("Image:   {:4d} x {:4d} = {} px\n", width, height, width * height);
+    const int num_blocks = blocks_dims.x * blocks_dims.y * blocks_dims.z;
+    print("Image:   {:4d} x {:4d} x {:2d} = {} px\n",
+          width,
+          height,
+          batch_size,
+          width * height * batch_size);
     print("Threads: {:4d} x {:<4d} = {}\n",
           thread_block_size.x,
           thread_block_size.y,
           num_threads_per_block);
-    print("Blocks:  {:4d} x {:<4d} = {}\n", blocks_dims.x, blocks_dims.y, num_blocks);
+    print("Blocks:  {:4d} x {:<4d} x {:2d} = {}\n",
+          blocks_dims.x,
+          blocks_dims.y,
+          blocks_dims.z,
+          num_blocks);
 
     // Create a host memory area to read the image into
     // auto host_image = std::make_unique<pixel_t[]>(width * height);
-    auto host_image = make_cuda_pinned_malloc<pixel_t>(width * height);
+    auto host_image = make_cuda_pinned_malloc<pixel_t>(width * height * batch_size);
 
     // Device-side pitched storage for image data
-    auto [dev_image, device_pitch] = make_cuda_pitched_malloc<pixel_t>(width, height);
+    auto [dev_image, device_pitch] =
+      make_cuda_pitched_malloc<pixel_t>(width, height * batch_size);
     auto [dev_mask, device_mask_pitch] =
-      make_cuda_pitched_malloc<uint8_t>(width, height);
+      make_cuda_pitched_malloc<uint8_t>(width, height * batch_size);
     print("Allocated device memory. Pitch = {} vs naive {}\n", device_pitch, width);
 
     // Managed memory areas for results
-    auto result_sum = make_cuda_managed_malloc<int>(device_pitch * height);
-    auto result_sumsq = make_cuda_managed_malloc<size_t>(device_pitch * height);
-    auto result_n = make_cuda_managed_malloc<uint8_t>(device_mask_pitch * height);
-    auto result_strong = make_cuda_managed_malloc<uint8_t>(device_mask_pitch * height);
+    auto result_sum = make_cuda_managed_malloc<int>(device_pitch * height * batch_size);
+    auto result_sumsq =
+      make_cuda_managed_malloc<size_t>(device_pitch * height * batch_size);
+    auto result_n =
+      make_cuda_managed_malloc<uint8_t>(device_mask_pitch * height * batch_size);
+    auto result_strong =
+      make_cuda_managed_malloc<uint8_t>(device_mask_pitch * height * batch_size);
     // Make sure to clear these completely
-    cudaMemset(result_sum.get(), 0, sizeof(int) * device_pitch * height);
-    cudaMemset(result_sumsq.get(), 0, sizeof(size_t) * device_pitch * height);
-    cudaMemset(result_n.get(), 0, sizeof(uint8_t) * device_mask_pitch * height);
-    cudaMemset(result_strong.get(), 0, sizeof(uint8_t) * device_mask_pitch * height);
+    cudaMemset(result_sum.get(), 0, sizeof(int) * device_pitch * height * batch_size);
+    cudaMemset(
+      result_sumsq.get(), 0, sizeof(size_t) * device_pitch * height * batch_size);
+    cudaMemset(
+      result_n.get(), 0, sizeof(uint8_t) * device_mask_pitch * height * batch_size);
+    cudaMemset(result_strong.get(),
+               0,
+               sizeof(uint8_t) * device_mask_pitch * height * batch_size);
     cudaDeviceSynchronize();
     cuda_throw_error();
 
@@ -189,25 +217,42 @@ int main(int argc, char **argv) {
     print("\nProcessing {} Images\n\n", reader.get_number_of_images());
     auto spotfinder = StandaloneSpotfinder(width, height);
 
-    for (size_t image_id = 0; image_id < reader.get_number_of_images(); ++image_id) {
-        if (args.image_number.has_value() && args.image_number.value() != image_id) {
+    for (size_t image_id = 0; image_id < reader.get_number_of_images();
+         image_id += batch_size) {
+        if (args.image_number.has_value()
+            && (args.image_number.value() < image_id
+                || args.image_number.value() >= image_id + batch_size)) {
             continue;
         }
 
-        print("Image {}:\n", image_id);
+        // How many images to do in this batch. May be less than batch size.
+        size_t num_images =
+          min(image_id + batch_size, reader.get_number_of_images()) - image_id;
+        blocks_dims.z = num_images;
+        size_t pixels_processed = width * height * num_images;
+
+        if (batch_size > 1) {
+            print("Images [{}-{}]:\n", image_id, image_id + num_images - 1);
+        } else {
+            print("Image  {}:\n", image_id);
+        }
+
         pre_load.record();
         pre_load.synchronize();
+        // print("Num images: {}\n", num_images);
+        for (size_t offset_id = 0; offset_id < num_images; ++offset_id) {
+            reader.get_image_into(image_id + offset_id,
+                                  host_image.get() + (width * height * offset_id));
+        }
 
-        reader.get_image_into(image_id, host_image.get());
-
-        // Copy the image to GPU
+        // Copy the image(s) to GPU
         start.record();
         cudaMemcpy2DAsync(dev_image.get(),
                           device_pitch * sizeof(pixel_t),
                           host_image.get(),
                           width * sizeof(pixel_t),
                           width * sizeof(pixel_t),
-                          height,
+                          height * num_images,
                           cudaMemcpyHostToDevice);
         memcpy.record();
         cuda_throw_error();
@@ -230,81 +275,98 @@ int main(int argc, char **argv) {
         print("    Read Time: \033[1m{:6.2f}\033[0m ms \033[37m{:>11}\033[0m\n",
               start.elapsed_time(pre_load),
               format("({:4.1f} GBps)",
-                     GBps<pixel_t>(start.elapsed_time(pre_load), width * height)));
+                     GBps<pixel_t>(start.elapsed_time(pre_load), pixels_processed)));
         print("  Upload Time: \033[1m{:6.2f}\033[0m ms \033[37m({:4.1f} GBps)\033[0m\n",
               memcpy.elapsed_time(start),
-              GBps<pixel_t>(memcpy.elapsed_time(start), width * height));
+              GBps<pixel_t>(memcpy.elapsed_time(start), pixels_processed));
         print("  Kernel Time: \033[1m{:6.2f}\033[0m ms \033[37m{:>11}\033[0m\n",
               kernel.elapsed_time(memcpy),
               format("({:.1f} GBps)",
-                     GBps<pixel_t>(kernel.elapsed_time(memcpy), width * height)));
+                     GBps<pixel_t>(kernel.elapsed_time(memcpy), pixels_processed)));
         print("               ════════\n");
         print("        Total: \033[1m{:6.2f}\033[0m ms {:>11}\n",
               all.elapsed_time(pre_load),
               format("({:.1f} GBps)",
-                     GBps<pixel_t>(all.elapsed_time(pre_load), width * height)));
+                     GBps<pixel_t>(all.elapsed_time(pre_load), pixels_processed)));
 
-        auto strong =
-          count_nonzero(result_strong.get(), width, height, device_mask_pitch);
+        auto strong = count_nonzero(result_strong.get(),
+                                    width,
+                                    static_cast<int>(height * num_images),
+                                    device_mask_pitch);
         print("       Strong: {} px\n", strong);
 
         auto start_time = std::chrono::high_resolution_clock::now();
         size_t mismatch_x = 0, mismatch_y = 0;
 
-        auto converted_image =
-          std::vector<double>{host_image.get(), host_image.get() + width * height};
-        auto dials_strong = spotfinder.standard_dispersion(
-          converted_image, reader.get_mask().value_or(span<uint8_t>{}));
-        auto end_time = std::chrono::high_resolution_clock::now();
-        size_t dials_results = count_nonzero(dials_strong, width, height, width);
-        float validation_time_ms =
-          std::chrono::duration_cast<std::chrono::duration<double>>(end_time
-                                                                    - start_time)
-            .count()
-          * 1000;
-        print("        Dials: {} px in {:.0f} ms CPU time\n",
-              dials_results,
-              validation_time_ms);
-        bool validation_matches = compare_results(dials_strong.data(),
-                                                  width,
-                                                  result_strong.get(),
-                                                  device_mask_pitch,
-                                                  width,
-                                                  height,
-                                                  &mismatch_x,
-                                                  &mismatch_y);
+        for (size_t offset_id = 0; offset_id < num_images; ++offset_id) {
+            size_t offset = width * height * offset_id;
 
-        if (validation_matches) {
-            print("     Compared: \033[32mMatch\033[0m\n");
-        } else {
-            print("     Compared: \033[1;31mMismatch\033[0m\n");
+            if (batch_size > 1) {
+                print("  Image {}:\n", image_id + offset_id);
+            }
+            // Read the image into a vector
+            auto converted_image =
+              std::vector<double>{host_image.get() + offset,
+                                  host_image.get() + width * height * (offset_id + 1)};
+            auto dials_strong = spotfinder.standard_dispersion(
+              converted_image, reader.get_mask().value_or(span<uint8_t>{}));
+            auto end_time = std::chrono::high_resolution_clock::now();
+            size_t dials_results = count_nonzero(dials_strong, width, height, width);
+            float validation_time_ms =
+              std::chrono::duration_cast<std::chrono::duration<double>>(end_time
+                                                                        - start_time)
+                .count()
+              * 1000;
+            print("        Dials: {} px in {:.0f} ms CPU time\n",
+                  dials_results,
+                  validation_time_ms);
+            bool validation_matches = compare_results(dials_strong.data(),
+                                                      width,
+                                                      result_strong.get() + offset,
+                                                      device_mask_pitch,
+                                                      width,
+                                                      height,
+                                                      &mismatch_x,
+                                                      &mismatch_y);
 
-            mismatch_x = max(static_cast<int>(mismatch_x) - 8, 0);
-            mismatch_y = max(static_cast<int>(mismatch_y) - 8, 0);
+            if (validation_matches) {
+                print("     Compared: \033[32mMatch\033[0m\n");
+            } else {
+                print("     Compared: \033[1;31mMismatch\033[0m\n");
 
-            print("Data:\n");
-            draw_image_data(host_image, mismatch_x, mismatch_y, 16, 16, width, height);
-            print("Strong From DIALS:\n");
-            draw_image_data(
-              dials_strong, mismatch_x, mismatch_y, 16, 16, width, height);
-            print("Strong From kernel:\n");
-            draw_image_data(
-              result_strong, mismatch_x, mismatch_y, 16, 16, device_mask_pitch, height);
-            // print("Resultant N:\n");
-            print("Sum From kernel:\n");
-            draw_image_data(
-              result_sum, mismatch_x, mismatch_y, 16, 16, device_pitch, height);
-            print("Sum² From kernel:\n");
-            draw_image_data(
-              result_sumsq, mismatch_x, mismatch_y, 16, 16, device_pitch, height);
-            print("Mask:\n");
-            draw_image_data(reader.get_mask().value().data(),
-                            mismatch_x,
-                            mismatch_y,
-                            16,
-                            16,
-                            width,
-                            height);
+                mismatch_x = max(static_cast<int>(mismatch_x) - 8, 0);
+                mismatch_y = max(static_cast<int>(mismatch_y) - 8, 0);
+
+                print("Data:\n");
+                draw_image_data(
+                  host_image, mismatch_x, mismatch_y, 16, 16, width, height);
+                print("Strong From DIALS:\n");
+                draw_image_data(
+                  dials_strong, mismatch_x, mismatch_y, 16, 16, width, height);
+                print("Strong From kernel:\n");
+                draw_image_data(result_strong,
+                                mismatch_x,
+                                mismatch_y,
+                                16,
+                                16,
+                                device_mask_pitch,
+                                height);
+                // print("Resultant N:\n");
+                print("Sum From kernel:\n");
+                draw_image_data(
+                  result_sum, mismatch_x, mismatch_y, 16, 16, device_pitch, height);
+                print("Sum² From kernel:\n");
+                draw_image_data(
+                  result_sumsq, mismatch_x, mismatch_y, 16, 16, device_pitch, height);
+                print("Mask:\n");
+                draw_image_data(reader.get_mask().value().data(),
+                                mismatch_x,
+                                mismatch_y,
+                                16,
+                                16,
+                                width,
+                                height);
+            }
         }
         print("\n\n");
     }
