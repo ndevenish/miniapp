78d542080e9c32976805f02ce4272484279a4363
Dimitri Vlachos <dimitri.j.vlachos@gmail.com>
2024-06-20 18:29:08 +0100

    Add host function for calling resolution filtering kernel

--- cuda/spotfinder/spotfinder.h
+++ cuda/spotfinder/spotfinder.h
@@ -12,6 +12,23 @@ constexpr int KERNEL_WIDTH = 3;
 /// One-direction height of kernel. Total kernel span is (K_H * 2 + 1)
 constexpr int KERNEL_HEIGHT = 3;
 
+void call_apply_resolution_mask(dim3 blocks,
+                                dim3 threads,
+                                size_t shared_memory,
+                                cudaStream_t stream,
+                                uint8_t *mask,
+                                size_t mask_pitch,
+                                int width,
+                                int height,
+                                float wavelength,
+                                float distance_to_detector,
+                                float beam_center_x,
+                                float beam_center_y,
+                                float pixel_size_x,
+                                float pixel_size_y,
+                                float dmin,
+                                float dmax);
+
 void call_do_spotfinding_naive(dim3 blocks,
                                dim3 threads,
                                size_t shared_memory,
