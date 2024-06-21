a83c1f32cba75a00cd6b26440e42b7970363e415
Dimitri Vlachos <dimitri.j.vlachos@gmail.com>
2024-06-21 17:52:41 +0100

    Create structs for res filtering data

--- cuda/spotfinder/spotfinder.h
+++ cuda/spotfinder/spotfinder.h
@@ -12,6 +12,58 @@ constexpr int KERNEL_WIDTH = 3;
 /// One-direction height of kernel. Total kernel span is (K_H * 2 + 1)
 constexpr int KERNEL_HEIGHT = 3;
 
+/**
+ * @brief Struct to store the geometry of the detector.
+*/
+struct detector_geometry {
+    float pixel_size_x;
+    float pixel_size_y;
+    float beam_center_x;
+    float beam_center_y;
+    float distance;
+
+    /**
+     * @brief Constructor to initialize the detector geometry from a JSON object.
+     * @param geometry_data A JSON object containing the detector geometry data.
+     * The JSON object must have the following keys:
+     * - pixel_size_x: The pixel size of the detector in the x-direction in mm
+     * - pixel_size_y: The pixel size of the detector in the y-direction in mm
+     * - beam_center_x: The x-coordinate of the pixel beam center in the image
+     * - beam_center_y: The y-coordinate of the pixel beam center in the image
+     * - distance: The distance from the sample to the detector in mm
+    */
+    detector_geometry(nlohmann::json geometry_data) {
+        std::vector<std::string> required_keys = {
+          "pixel_size_x", "pixel_size_y", "beam_center_x", "beam_center_y", "distance"};
+
+        for (const auto &key : required_keys) {
+            if (geometry_data.find(key) == geometry_data.end()) {
+                throw std::invalid_argument("Key " + key
+                                            + " is missing from the input JSON");
+            }
+        }
+
+        pixel_size_x = geometry_data["pixel_size_x"];
+        pixel_size_y = geometry_data["pixel_size_y"];
+        beam_center_x = geometry_data["beam_center_x"];
+        beam_center_y = geometry_data["beam_center_y"];
+        distance = geometry_data["distance"];
+    }
+};
+
+/**
+ * @brief Struct to store parameters for calculating the resolution filtered mask
+*/
+struct ResolutionMaskParams {
+    size_t mask_pitch;
+    int width;
+    int height;
+    float wavelength;
+    detector_geometry detector;
+    float dmin;
+    float dmax;
+};
+
 void call_apply_resolution_mask(dim3 blocks,
                                 dim3 threads,
                                 size_t shared_memory,
