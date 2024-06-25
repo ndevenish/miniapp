ef0a35791e387aa0477018ec57aab95ceea5c2cf
Dimitri Vlachos <dimitri.j.vlachos@gmail.com>
2024-06-25 15:11:23 +0100

    Current testing params

--- cuda/spotfinder/tests/res_comparison.sh
+++ cuda/spotfinder/tests/res_comparison.sh
@@ -19,12 +19,9 @@ exec 3> output_file.txt
   --min-spot-size 3 \
   --pipe_fd 3 \
   --dmin 4 \
-  --dmax 40 \
   --wavelength 0.976261 \
   --detector "$(cat detector.json)" \
   --threads 1 \
-  --images 10 \
-  --writeout
 
 # Close file descriptor 3
 exec 3>&-
