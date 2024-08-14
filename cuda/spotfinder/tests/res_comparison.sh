a215204085d0c301499c065a96250c6f9a2e40fb
Dimitri Vlachos <dimitri.j.vlachos@gmail.com>
2024-08-14 18:33:47 +0100

    Add new dataset test

--- cuda/spotfinder/tests/res_comparison.sh
+++ cuda/spotfinder/tests/res_comparison.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
 # Create a JSON file
+# cat << EOF > detector.json
+# {
+#     "pixel_size_x": 0.075,
+#     "pixel_size_y": 0.075,
+#     "distance": 150,
+#     "beam_center_x": 1008.13,
+#     "beam_center_y": 1066.0
+# }
+# EOF
 cat << EOF > detector.json
 {
     "pixel_size_x": 0.075,
     "pixel_size_y": 0.075,
-    "distance": 150,
-    "beam_center_x": 1008.13,
-    "beam_center_y": 1066.0
+    "distance": 306.765,
+    "beam_center_x": 1597.11,
+    "beam_center_y": 1691.12
 }
 EOF
 
@@ -16,14 +25,23 @@ exec 3> output_file.txt
 
 # Run the program with file descriptor 3
 
-# Extended dispersion test dataset
-# ./spotfinder /dls/i24/data/2024/nr27313-319/gw/Test_Insulin/ins_big_15/ins_big_15_2_master.h5 \
 # Resoltuion filterting test dataset
-./spotfinder /dls/i03/data/2024/cm37235-2/xraycentring/TestInsulin/ins_14/ins_14_24.nxs \
+# ./spotfinder /dls/i03/data/2024/cm37235-2/xraycentring/TestInsulin/ins_14/ins_14_24.nxs \
+#   --min-spot-size 3 \
+#   --pipe_fd 3 \
+#   --dmin 4 \
+#   --wavelength 0.976261 \
+#   --detector "$(cat detector.json)" \
+#   --algorithm "dispersion_extended" \
+#   --images 1 \
+#   --writeout
+
+# Extended dispersion test dataset
+./spotfinder /dls/i24/data/2024/nr27313-319/gw/Test_Insulin/ins_big_15/ins_big_15_2_master.h5 \
   --min-spot-size 3 \
   --pipe_fd 3 \
   --dmin 4 \
-  --wavelength 0.976261 \
+  --wavelength 0.619901 \
   --detector "$(cat detector.json)" \
   --algorithm "dispersion_extended" \
   --images 1 \
